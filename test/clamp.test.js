0b9a604fa20b6f5411e2da36be5daf349d00192a
ViliVK <vilikujansuu@yahoo.com>
2023-12-06 23:29:34 +0200

    Added tests for the clamp function.

---
+++ test/clamp.test.js
@@ -0,0 +1,36 @@
+var chai = require('chai');
+var clamp = require('../clamp.js');
+var expect = chai.expect;
+
+describe('Clamp', () => {
+    it('does not modify values between the bounds', () => {
+        const result = clamp(9, 1, 12);
+        expect(result).to.equal(9)
+    });
+    it('clamps too low values to the lower bound', () => {
+        const result = clamp(2, 5, 12);
+        expect(result).to.equal(5)
+    });
+    it('clamps too high values to the upper bound', () => {
+        const result = clamp(100, 5, 12);
+        expect(result).to.equal(12)
+    });
+    it('handles float values', () => {
+        const result = clamp(5.2, 9.41, 12.1231);
+        expect(result).to.equal(9.41)
+    });
+    it('handles negative values', () => {
+        const result = clamp(-3, -23, -5);
+        expect(result).to.equal(-3);
+    });
+    it('handles extremely large numbers', () => {
+        const result = clamp(Number.MAX_SAFE_INTEGER, 2, 10);
+        expect(result).to.equal(10);
+    });
+    it('sends an RangeError when the lower and upper bound are switched', () => {
+        expect(() => clamp(9, 12, 5)).to.throw(RangeError, 'The bounds are not acceptable.');
+    });
+    it('sends a TypeError when you try to clamp something that is not a number', () => {
+        expect(() => clamp('c', 'a', 'd')).to.throw(TypeError, 'Only for numbers >:(');
+    });
+});
